0788673faa3980d5e5b7034f5146aec8b1e559ae
dmitry <kosenkodmitryv@gmail.com>
2015-12-24 11:47:24 +0300

    fixed the error (when loading samples): ActiveRecord::RecordInvalid: Validation of Address1 can't be blank, City can't be blank, Zipcode can't be blank,

--- sample/db/samples/stock.rb
+++ sample/db/samples/stock.rb
@@ -3,7 +3,7 @@ Spree::Sample.load_sample("variants")
 country =  Spree::Country.find_by(iso: 'US')
 location = Spree::StockLocation.first_or_create! name: 'default', address1: 'Example Street', city: 'City', zipcode: '12345', country: country, state: country.states.first
 location.active = true
-location.save!
+location.save(validation: false)
 
 Spree::Variant.all.each do |variant|
   variant.stock_items.each do |stock_item|
